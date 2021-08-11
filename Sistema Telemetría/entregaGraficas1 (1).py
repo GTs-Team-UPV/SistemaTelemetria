20aa8315b74c731127802d45dd4387e55b32e320
aucejito <38977418+aucejito@users.noreply.github.com>
2021-08-11 19:41:22 +0200

    Sistema Telemetría v1
    Falta integrar datos simulador v1

---
+++ Sistema Telemetría/entregaGraficas1 (1).py
@@ -0,0 +1,172 @@
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+from pandas.core.base import DataError
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+import pandas as pd
+import dash_daq as daq
+import numpy as np
+import plotly.express as px
+
+X = deque(maxlen = 40)
+X2 = deque(maxlen = 40)
+X3 = deque(maxlen = 20)
+X4 = deque(maxlen = 40)
+Y = deque(maxlen = 20)
+Y2 = deque(maxlen = 20)
+Y3 = deque(maxlen = 20)
+Y4 = deque(maxlen = 300)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        dcc.Graph(id = 'live-graph', animate = True),
+        dcc.Graph(id = 'live-graph2', animate = True),
+        dcc.Graph(id = 'live-graph3', animate = True),
+        daq.Gauge(  
+			color="#DC3912",
+			showCurrentValue=True,
+			units="RPS",
+			id='gauge',
+			label="Revoluciones",
+			max = 8000,
+			min = 0,
+			value=0
+		),
+        daq.Tank(
+			value=100,
+            color="#FF9900",
+			id = 'tank',
+			showCurrentValue=True,
+			units='litros',
+			max = 100,
+            min = 0,
+			style={'margin': 'auto', 'textAlign': 'center'}
+		),
+        dcc.Graph(id = 'live-graph4', animate = True , style = {'center' : 'auto'}),
+        dcc.Interval(
+			id = 'graph-update',
+			interval = 1000,
+			n_intervals = 0
+		),
+    ]
+)
+
+@app.callback(
+	Output('live-graph', 'figure'),
+	[ Input('graph-update', 'n_intervals') ]
+)
+def update_graph_scatter(n):
+		data = pd.read_csv('data.csv')
+		X.append(data['xlength'][n])
+		Y.append(data['vel'][n])
+		
+		graph = go.Scatter(
+			x=list(X),
+			y=list(Y),
+			name='Scatter',
+			mode= 'lines+markers',
+			line=dict(color="#0674D5")
+		)
+
+		return {'data': [graph],
+				'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)], title = 'Longitud Recorrida (m)'),
+                yaxis = dict(range = [min(Y),max(Y)], title = 'Velocidad (Km/h)'),
+                title = 'SPEED')}
+
+
+@app.callback(
+	Output('live-graph2', 'figure'),
+	[ Input('graph-update', 'n_intervals') ]
+)
+def update_graph_scatter(n):
+		data = pd.read_csv('data.csv')
+		Y2.append(data['fren'][n])		
+		graph = go.Scatter(
+			x=list(X),
+			y=list(Y2),
+			name='Scatter',
+			mode= 'lines+markers',
+			line=dict(color="#F55643")
+		)
+
+		
+
+		return {'data': [graph],
+				'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)], title = 'Longitud Recorrida (m)'),
+                yaxis = dict(range = [min(Y2),max(Y2)], title = 'Presión de Frenada (atm)'),
+                title = 'FRENADA')}  
+
+
+@app.callback(
+	Output('live-graph3', 'figure'),
+	[ Input('graph-update', 'n_intervals') ]
+)
+def update_graph_scatter(n):
+		data = pd.read_csv('data.csv')
+		Y3.append(data['marcha'][n])
+		
+		graph = go.Scatter(
+			x=list(X),
+			y=list(Y3),
+			name='Scatter',
+			mode= 'lines+markers',
+			line=dict(color="#03D750")
+		)
+
+		return {'data': [graph],
+				'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)], title = 'Longitud Recorrida (m)'),
+                yaxis = dict(range = [min(Y3),max(Y3)], title = 'Marcha Actual'),
+                title = 'MARCHA')}
+
+
+@app.callback(
+	Output('gauge', 'value'),
+	[ Input('graph-update', 'n_intervals') ]
+)
+def update_gauge(n):
+	data = pd.read_csv('data.csv')
+	return data['revact'][n]
+
+
+@app.callback(
+	Output('tank', 'value'),
+	[ Input('graph-update', 'n_intervals')]
+)
+def update_output(n):
+	data = pd.read_csv('data.csv')
+	return int(data['comb'][n])
+
+
+
+@app.callback(
+	Output('live-graph4', 'figure'),
+	[ Input('graph-update', 'n_intervals') ]
+)
+def update_graph_scatter(n):
+		data = pd.read_csv('data.csv')
+		X4.append(data['xtime'][n])
+		Y4.append(data['comb'][n])
+		
+		graph = go.Scatter(
+			x=list(X),
+			y=list(Y4),
+			name='Scatter',
+            fill = 'tozeroy',
+            fillcolor = '#FF7F0E',
+			mode= 'lines'
+		)
+
+		return {'data': [graph],
+				'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)], title = 'TIME (s)'),
+                yaxis = dict(range = [min(Y4),max(Y4)], title = 'COMB ( % )'),
+                title = '% COMB / TIME')}
+
+if __name__ == '__main__':
+	app.run_server(debug = True)
+
