0a15b10d414eebb2ba047075bc522ee42e90368e
Carlos Gálvez <cargalau@inf.upv.es>
2022-03-03 10:15:49 +0100

    Un poco más receptor

--- dash_webapp/receptor.py
+++ dash_webapp/receptor.py
@@ -7,10 +7,17 @@ import time
 ser = serial.Serial('COM10', 57600, timeout=15, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 ser.write("mac pause\r\n".encode())
 print(ser.readline())
+ser.write("radio get freq\r\n".encode())
+print(ser.readline())
 radio_decoded = 'radio_err\r\n'
+fieldnames = ["xlength" , "vel" , "fren" ,"rpm", "marcha", "comb" , "xtime"]
 
 def listen():
     global radio_decoded
+    
+    with open('received_data.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
     while True:
         while radio_decoded == 'radio_err\r\n':
             ser.write("radio rx 0\r\n".encode())
@@ -21,12 +28,16 @@ def listen():
         #Cuando se salga del bucle, tendremos en radio 
         #decoded el data bueno que habrá que tratar
         #A lo mejor hay que quitar el radio rx antes de la trama
+        radio_decoded = radio_decoded[10:]
+        radio_decoded = radio_decoded[:40]
         hexList = reordenar_trama(radio_decoded)
         floatList = []
         for item in hexList:
             itemFloat = struct.unpack('!f', bytes.fromhex(item))[0]
             floatList.append(itemFloat)
         append_CSV(floatList)
+        radio_decoded = 'radio_err\r\n'
+
             
 
 
@@ -34,20 +45,15 @@ def append_CSV(floatList):
     xlength = vel = fren = marcha = rpm = xtime = 0
     comb = 100
     print(floatList)
-    fieldnames = ["xlength" , "vel" , "fren" ,"rpm", "marcha", "comb" , "xtime"]
-    
-    with open('received_data.csv', 'w') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        csv_writer.writeheader()
-    
-    with open('received_data.csv', 'a') as csv_file:
+    global fieldnames
+    with open('received_data.csv', 'a',newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         info = {
             "xlength" : round(floatList[0],2),
             "vel" : round(floatList[1],2),
             "fren" : round(floatList[2],2),
             "rpm" : round(floatList[3],2),
-            "marcha" : floatList[4],
+            "marcha" : int(floatList[4]),
             "comb" : comb,
             "xtime" : xtime
         }
